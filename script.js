e03d951d7e5795fbb91937f91dc6877d44444128
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-12-26 12:07:58 +0100

    C5 : fond d’écran en violet

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const button = document.querySelector("button");
+const body = document.querySelector("body");
 
-const background_color = "gray";
+const background_color = "purple";
 const border = "yellow";
 const text = "black";
 
+body.style.backgroundColor = background_color;
 button.style.backgroundColor = background_color;
 button.style.color = text;
 button.style.border = `4px solid ${border}`;
