780220afe47dde1c2fc1dfd502c13ae1544afc27
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-12-26 12:06:19 +0100

    C3 : mise à jour des couleurs de text, border et background  et ajustement du texte des constantes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const button = document.querySelector("button");
 
-const background = "red";
-const border = "blue";
-const text = "white";
+const background_color = "gray";
+const border = "yellow";
+const text = "black";
 
-button.style.backgroundColor = background;
+button.style.backgroundColor = background_color;
 button.style.color = text;
 button.style.border = `4px solid ${border}`;
